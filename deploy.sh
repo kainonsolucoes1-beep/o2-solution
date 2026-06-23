9624fe23242d84742e2d08961d3527b862c65465
O2 Solution <kainonsolucoes1@gmail.com>
2026-06-23 15:41:54 -0300

    fix: remove --no-cache do deploy para builds mais rapidos

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 set -e
 cd ~/o2-solution
 git pull
-sudo docker-compose -f docker-compose.prod.yml build --no-cache
+sudo docker-compose -f docker-compose.prod.yml build
 sudo docker-compose -f docker-compose.prod.yml down
 sudo docker-compose -f docker-compose.prod.yml up -d
